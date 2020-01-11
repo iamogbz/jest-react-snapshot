c93e48a2a78484455248261e6b3ece3d3547bc88
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-01-10 22:34:36 -0500

    build: copy readme and types

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,12 @@
 import * as path from "path";
 import { Configuration } from "webpack";
+import * as CopyPlugin from "copy-webpack-plugin";
 
 const configuration: Configuration = {
     devtool: "source-map",
     entry: "./src",
     mode: "production",
+    target: "node",
     module: {
         rules: [
             {
@@ -19,13 +21,17 @@ const configuration: Configuration = {
             },
         ],
     },
+    plugins: [
+        new CopyPlugin(["package.json", "README.md"]),
+        new CopyPlugin([{ from: "src/@types" }]),
+    ],
     output: {
         filename: "main.js",
         libraryTarget: "commonjs",
         path: path.resolve(__dirname, "lib"),
     },
     resolve: {
-        extensions: [".js", ".ts"],
+        extensions: [".js", ".ts", ".jsx", ".tsx"],
         modules: [path.resolve("./src"), path.resolve("./node_modules")],
     },
 };
