7f0326ee64f1da8d75bc3b450ede9b4416ec605a
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-01-10 20:40:23 -0500

    chore: remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import { renderToString } from "react-dom/server";
-
-import {
-    ServerStyleSheet,
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    __DO_NOT_USE_OR_YOU_WILL_BE_HAUNTED_BY_SPOOKY_GHOSTS,
-} from "styled-components";
 import {
     configureToMatchImageSnapshot,
     MatchImageSnapshotOptions,
