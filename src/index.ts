d42d8f3cc1dffa34ac5a49968377d63e39c7303f
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-01-10 22:08:42 -0500

    chore: relax default match options

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,20 @@ import {
 import { styledHtml } from "./react-html";
 import { htmlImage } from "./html-image";
 
+const defaultOptions: MatchImageSnapshotOptions = {
+    blur: 2,
+    customDiffConfig: { threshold: 0.5 },
+    failureThreshold: 0.05,
+    failureThresholdType: "percent",
+};
+
 export async function imageToMatchSnapshot(
     recieved: React.ReactElement,
-    options: MatchImageSnapshotOptions,
+    options: MatchImageSnapshotOptions = {},
 ): Promise<jest.CustomMatcherResult> {
     const image = await htmlImage(styledHtml(recieved));
-    return configureToMatchImageSnapshot(options).bind(this)(image);
+    return configureToMatchImageSnapshot({
+        ...defaultOptions,
+        ...options,
+    }).bind(this)(image);
 }
